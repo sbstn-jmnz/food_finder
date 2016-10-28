4e1dcc35dc497e63a9de88b758c7c623d17e0e91
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-10-28 15:39:11 -0300

    number_helper.rb tested

--- spec/support/number_helper_spec.rb
+++ spec/support/number_helper_spec.rb
@@ -1,41 +1,72 @@
 # encoding: UTF-8
-
 describe 'NumberHelper' do
 
   include NumberHelper
-  
+
   describe '#number_to_currency' do
-    
+
     context 'using default values' do
-      
-      it "correctly formats an integer"
-      
-      it "correctly formats a float"
 
-      it "correctly formats a string"
-      
-      it "uses delimiters for very large numbers"
+      it "correctly formats an integer" do
+        num = number_to_currency(3)
+        expect(num).to eq('$3.00')
+      end
 
-      it "does not have delimiters for small numbers"
+      it "correctly formats a float" do
+        num = number_to_currency(3.1)
+        expect(num).to eq('$3.10')
+      end
+
+      it "correctly formats a string" do
+        num = number_to_currency('3.1')
+        expect(num).to eq('$3.10')
+      end
+
+      it "uses delimiters for very large numbers" do
+        num = number_to_currency('31000')
+        expect(num).to eq('$31,000.00')
+      end
+
+      it "does not have delimiters for small numbers" do
+        num = number_to_currency(3)
+        expect(num).to eq('$3.00')
+      end
 
     end
-    
+
     context 'using custom options' do
-      
-      it 'allows changing the :unit'
 
-      it 'allows changing the :precision'
-      
-      it 'omits the separator if :precision is 0'
-      
-      it 'allows changing the :delimiter'
-      
-      it 'allows changing the :separator'
-            
-      it 'correctly formats using multiple options'
-      
+      it 'allows changing the :unit' do
+        num = number_to_currency 3, unit: '#'
+        expect(num).to eq('#3.00')
+      end
+
+      it 'allows changing the :precision' do
+        num = number_to_currency 3, precision: 1
+        expect(num).to eq('$3.0')
+      end
+
+      it 'omits the separator if :precision is 0' do
+        num = number_to_currency 3, precision: 0
+        expect(num).to eq('$3')
+      end
+
+      it 'allows changing the :delimiter' do
+        num = number_to_currency 3000, delimiter: '.'
+        expect(num).to eq('$3.000.00')
+      end
+
+      it 'allows changing the :separator' do
+        num = number_to_currency 3000, separator: ','
+        expect(num).to eq('$3,000,00')
+      end
+
+      it 'correctly formats using multiple options' do
+        num = number_to_currency 3000, separator: ',', delimiter: '.'
+        expect(num).to eq('$3.000,00')
+      end
     end
-    
+
   end
-  
+
 end
